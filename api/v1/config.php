7f8dd2e6d5aafb87694140b9b1d3e38a0d4f2766
DIMABAHTIN <dbahtin@yandex.ru>
2016-08-08 14:23:52 +0700

    Initial commit

---
+++ api/v1/config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: espe
+ * Date: 03.08.2016
+ * Time: 23:15
+ */
+
+/* MySQL config */
+define('MYSQL_SERVER', 'localhost');
+define('MYSQL_USER', 'testuser');
+define('MYSQL_PASSWORD', '');
+define('MYSQL_DB', 'testservicedb');
+define('MYSQL_CHARSET', 'utf8');
+
+/* time config */
+date_default_timezone_set('Asia/Krasnoyarsk');
+
+// debug ON || OFF
+define('DEBUG', '0');
+
